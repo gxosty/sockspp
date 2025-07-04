a011e8fab843fb7564770b0fd36dc94b570890fc
gxosty <6321b5arsi@gmail.com>
2025-07-04 23:45:36 +0500

    Added default params, `dns_ip` and `dns_port` fields

--- src/server/src/sockspp/server/server_params.hpp
+++ src/server/src/sockspp/server/server_params.hpp
@@ -9,9 +9,11 @@ namespace sockspp
 struct ServerParams
 {
     std::string listen_ip;
-    uint16_t listen_port;
+    uint16_t listen_port = 1080;
     std::string username;
     std::string password;
+    std::string dns_ip;
+    uint16_t dns_port = 53;
 }; // class ServerParams
 
 } // namespace sockspp
